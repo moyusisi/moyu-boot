ff60c4101ceb122b06ca4c4d8d0c7bbbb6d93010
shisong <rgkjhshi@gmail.com>
2025-09-14 17:32:42 +0800

    数据表查询

--- src/main/java/com/moyu/boot/plugin/codegen/model/param/TableQueryParam.java
+++ src/main/java/com/moyu/boot/plugin/codegen/model/param/TableQueryParam.java
@@ -8,7 +8,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
-import java.util.Set;
 
 /**
  * 表查询参数
@@ -20,14 +19,8 @@ import java.util.Set;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TableQueryParam extends BasePageParam {
-    //********** 额外字段 **********//
     /**
-     * 待删除的id列表，通常不会集联删除
-     */
-    private Set<Long> ids;
-
-    /**
-     * 名称关键词
+     * 搜索关键词
      */
     private String searchKey;
 
@@ -36,10 +29,4 @@ public class TableQueryParam extends BasePageParam {
      */
     @JsonIgnore
     private List<String> excludeTables;
-
-    //********** db中存在的字段 **********//
-    /**
-     * 主键id
-     */
-    private Long id;
 }
