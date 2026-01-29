bb627e244d3b4bd7d7d6b330c924b3e59d954b2e
shisong <rgkjhshi@gmail.com>
2026-01-29 17:27:51 +0800

    Create IntradayIdController.java

---
+++ src/main/java/com/moyu/boot/plugin/intradayId/controller/IntradayIdController.java
@@ -0,0 +1,34 @@
+package com.moyu.boot.plugin.intradayId.controller;
+
+import com.moyu.boot.common.core.annotation.Log;
+import com.moyu.boot.common.core.model.Result;
+import com.moyu.boot.plugin.intradayId.service.IntradayIdService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * 日内标识控制器
+ *
+ * @author moyusisi
+ * @since 2025-11-15
+ */
+@Log(jsonLog = true)
+@RestController
+@RequestMapping("/api/dev/seq")
+public class IntradayIdController {
+
+    @Resource
+    private IntradayIdService intradayIdService;
+
+    /**
+     * 自增测试接口
+     */
+    @PostMapping("/inc")
+    public Result<String> inc() {
+        String sn = intradayIdService.nextId("SN", 5);
+        return Result.success(sn);
+    }
+}
