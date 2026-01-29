7834f0486eeb92e7c68a933d23d7af31df1850f9
shisong <rgkjhshi@gmail.com>
2026-01-29 16:55:56 +0800

    Update IntradayIdServiceImpl.java

--- src/main/java/com/moyu/boot/plugin/intradayId/service/impl/IntradayIdServiceImpl.java
+++ src/main/java/com/moyu/boot/plugin/intradayId/service/impl/IntradayIdServiceImpl.java
@@ -1,5 +1,6 @@
 package com.moyu.boot.plugin.intradayId.service.impl;
 
+import cn.hutool.core.util.StrUtil;
 import com.moyu.boot.plugin.intradayId.service.IntradayIdService;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
@@ -31,29 +32,55 @@ public class IntradayIdServiceImpl implements IntradayIdService {
     @Override
     public String nextId() {
         String today = DTF.format(LocalDate.now());
-        String key = INTRADAY_SEQ + today;
-        // Redis 原子递增（初始值为 0，第一次递增后返回 1，后续依次+1）
-        Long increment = redisTemplate.opsForValue().increment(key, 1);
-        // 设置过期时间（仅第一次递增时设置，避免重复设置）
-        if (increment != null && increment == 1) {
-            redisTemplate.expire(key, 24, TimeUnit.HOURS);
-        }
-        // 格式化补零：不足指定长度左侧补 0
-        return today + String.format("%04d", increment);
+        Long seq = generatorId(null, today);
+        // 4. 格式化补零，返回日内标识
+        return today + String.format("%04d", seq);
     }
 
     @Override
     public String nextId(Integer size) {
-        return "";
+        String today = DTF.format(LocalDate.now());
+        Long seq = generatorId(null, today);
+        // 4. 格式化补零，返回日内标识
+        return today + String.format("%0" + size + "d", seq);
     }
 
     @Override
     public String nextId(String prefix) {
-        return "";
+        String today = DTF.format(LocalDate.now());
+        Long seq = generatorId(prefix, today);
+        // 4. 格式化补零，返回日内标识
+        return today + String.format("%04d", seq);
     }
 
     @Override
     public String nextId(String prefix, Integer size) {
-        return "";
+        String today = DTF.format(LocalDate.now());
+        Long seq = generatorId(prefix, today);
+        // 4. 格式化补零，返回日内标识
+        return today + String.format("%0" + size + "d", seq);
     }
+
+    /**
+     * 生成日内递增标识（纯数字，无补零）
+     *
+     * @return 当日递增数字（例如 1、2、3...）
+     */
+    public Long generatorId(String prefix, String today) {
+        // 1. 构造当日的key,格式为: intraday:seq:[prefix:]today
+        String key = INTRADAY_SEQ;
+        if (StrUtil.isNotEmpty(prefix)) {
+            key = INTRADAY_SEQ + prefix + ":";
+        }
+        key = key + today;
+        // 2. redis原子递增（初始值为 0，第一次递增后返回 1，后续依次+1）
+        Long increment = redisTemplate.opsForValue().increment(key, 1);
+        // 3. 设置过期时间（仅第一次递增时设置，避免重复设置）
+        if (increment != null && increment == 1) {
+            redisTemplate.expire(key, 24, TimeUnit.HOURS);
+        }
+        // 4. 日内递增序列值
+        return increment;
+    }
+
 }
