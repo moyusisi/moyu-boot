6de7ab896a4c3cbf5879b5221a66a9f57398dcac
shisong <rgkjhshi@gmail.com>
2026-03-06 11:41:15 +0800

    Update moyu_mysql.sql

--- sql/mysql/moyu_mysql.sql
+++ sql/mysql/moyu_mysql.sql
@@ -184,8 +184,8 @@ CREATE TABLE `sys_log`
 (
     `id`               BIGINT NOT NULL AUTO_INCREMENT COMMENT '主键id',
     `name`             VARCHAR(50)  DEFAULT NULL COMMENT '日志名称',
-    `module`           VARCHAR(50)  DEFAULT NULL COMMENT '系统/模块',
     `log_type`         TINYINT      DEFAULT NULL COMMENT '日志类型(字典 0默认日志 1访问日志 2操作日志 3交互日志)',
+    `module`           VARCHAR(50)  DEFAULT NULL COMMENT '系统/模块',
     `business`         VARCHAR(50)  DEFAULT NULL COMMENT '业务',
     `operate`          VARCHAR(50)  DEFAULT NULL COMMENT '操作/接口',
     `content`          VARCHAR(255) DEFAULT NULL COMMENT '内容说明',
