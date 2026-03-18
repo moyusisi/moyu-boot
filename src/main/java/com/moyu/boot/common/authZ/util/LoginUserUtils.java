d2c076590e7b702823d795c7160b0505340a771c
shisong <rgkjhshi@gmail.com>
2026-03-18 11:40:25 +0800

    LoginUserUtils添加LOCAL_USER

--- src/main/java/com/moyu/boot/common/authZ/util/LoginUserUtils.java
+++ src/main/java/com/moyu/boot/common/authZ/util/LoginUserUtils.java
@@ -23,13 +23,25 @@ public class LoginUserUtils {
     public static final String ROOT_ROLE = "ROOT";
 
     /**
+     * 存储当前线程的 LoginUser（线程隔离）
+     */
+    private static final ThreadLocal<LoginUser> LOCAL_USER = new ThreadLocal<>();
+
+    /**
      * 获取当前登录用户信息
      **/
     public static Optional<LoginUser> getLoginUser() {
         Optional<LoginUser> optUser = Optional.empty();
+        // 先检查登录，否则StpUtil.getTokenSession()时会校验是否登录，无则抛出异常
         if (StpUtil.isLogin()) {
-            // StpUtil.getTokenSession()时会校验是否登录(无则抛出异常)
-            optUser = Optional.of((LoginUser) StpUtil.getTokenSession().get("loginUser"));
+            // 1.优先从线程变量中获取LoginUser
+            LoginUser loginUser = LOCAL_USER.get();
+            // 2.无则从缓存中获取LoginUser，并存入线程变量
+            if (loginUser == null) {
+                loginUser = (LoginUser) StpUtil.getTokenSession().get("loginUser");
+                LOCAL_USER.set(loginUser);
+            }
+            optUser = Optional.of(loginUser);
         }
         return optUser;
     }
@@ -86,4 +98,11 @@ public class LoginUserUtils {
         return getRoles().contains(ROOT_ROLE);
     }
 
+    /**
+     * 清理当前线程的 ThreadLocal 数据（关键：防止内存泄漏）
+     * 建议在请求结束时调用（如 Spring 拦截器的 afterCompletion 方法）
+     */
+    public static void clearLoginUser() {
+        LOCAL_USER.remove();
+    }
 }
