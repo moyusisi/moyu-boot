65916273e2653fd40c4e5f11f0665f6a047d6a90
shisong <rgkjhshi@gmail.com>
2026-05-28 15:42:00 +0800

    系统配置表sys_config

--- sql/mysql/moyu_mysql.sql
+++ sql/mysql/moyu_mysql.sql
@@ -207,3 +207,28 @@ CREATE TABLE `sys_log`
 ) ENGINE = InnoDB
   CHARACTER SET = utf8mb4
   COLLATE = utf8mb4_general_ci COMMENT ='系统日志表';
+
+-- 8. 系统配置表（可选）
+drop table if exists sys_config;
+create table sys_config
+(
+    `id`          BIGINT NOT NULL AUTO_INCREMENT COMMENT '主键id',
+    `key_title`   VARCHAR(64) DEFAULT NULL COMMENT '配置项',
+    `key_name`    VARCHAR(64) DEFAULT NULL COMMENT '配置name',
+    `key_value`   TEXT        DEFAULT NULL COMMENT '配置value',
+    `key_type`    VARCHAR(64) DEFAULT NULL COMMENT '配置项类型',
+
+    `status`      TINYINT     DEFAULT 0 COMMENT '使用状态（0正常 1停用）',
+    `ext_json`    TEXT        DEFAULT NULL COMMENT '扩展信息',
+    `remark`      TEXT        DEFAULT NULL comment '备注',
+    `deleted`     TINYINT     DEFAULT 0 COMMENT '删除标志（0未删除  1已删除）',
+    `create_time` DATETIME    DEFAULT NULL COMMENT '创建时间',
+    `create_by`   VARCHAR(32) DEFAULT NULL COMMENT '创建人',
+    `update_time` DATETIME    DEFAULT NULL COMMENT '修改时间',
+    `update_by`   VARCHAR(32) DEFAULT NULL COMMENT '修改人',
+    primary key (`id`),
+    UNIQUE INDEX `uniq_key_name` (`key_name`)
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci
+  AUTO_INCREMENT = 100 COMMENT = '系统配置表';
