5672d9abc5fe9f7c70c443707427b97f6ab474f6
shisong <rgkjhshi@gmail.com>
2026-07-10 13:04:41 +0800

    Update init_data.sql

--- sql/mysql/init_data.sql
+++ sql/mysql/init_data.sql
@@ -65,9 +65,9 @@ values  ('0', '系统模块', 'sys_module', 1, '/sysModule', 'Layout', 'appstore
 
         ('dir_sys_ops', '会话管理', 'menu_sys_session', 3, '/auth/monitor', 'auth/monitor/index', 'bars-outlined', null, 1, null, 'sys_module', 5002, null, '', 0, null, null, null, null),
         ('dir_sys_ops', '序列器', 'menu_sys_seq', 3, '/dev/seq', 'dev/seq/index', 'field-number-outlined', null, 1, null, 'sys_module', 5003, null, '', 0, null, null, null, null),
-        ('dir_sys_ops', '三方用户', 'menu_sys_third', 3, '/auth/third', 'auth/third/index', 'team-outlined', null, 1, null, 'sys_module', 5010, null, '', 0, null, null, null, null),
+        ('dir_sys_ops', '系统配置', 'menu_sys_config', 3, '/sys/config', 'system/config/index', 'cloud-server-outlined', null, 1, null, 'sys_module', 5010, null, '', 0, null, null, null, null),
         ('dir_sys_ops', '任务调度', 'menu_sys_job', 3, '/dev/job', 'dev/job/index', 'field-time-outlined', null, 1, null, 'sys_module', 5020, null, '', 0, null, null, null, null),
-        ('dir_sys_ops', '系统配置', 'menu_sys_config', 3, '/sys/config', 'system/config/index', 'cloud-server-outlined', null, 1, null, 'sys_module', 5030, null, '', 0, null, null, null, null),
+        ('dir_sys_ops', '三方用户', 'menu_sys_third', 3, '/auth/third', 'auth/third/index', 'team-outlined', null, 1, null, 'sys_module', 5030, null, '', 0, null, null, null, null),
         ('dir_sys_ops', '内部链接', 'iframe_sys_link', 4, 'https://www.baidu.com', '', 'disconnect-outlined', '', 1, null, 'sys_module', 5040, null, '', 0, null, null, null, null),
         ('dir_sys_ops', '外部链接', 'link_sys_doc', 5, 'https://www.baidu.com', '', 'link-outlined', '', 1, null, 'sys_module', 5050, null, '', 0, null, null, null, null),
         ('dir_sys_ops', '日志审计', 'dir_sys_log', 2, '/ops/log', null, 'file-search-outlined', null, 1, null, 'sys_module', 5060, null, '', 0, null, null, null, null),
