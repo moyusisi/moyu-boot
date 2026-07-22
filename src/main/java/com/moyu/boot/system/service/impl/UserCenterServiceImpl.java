76557c041178dea3fb389ebc148d2fa2664ecd25
shisong <rgkjhshi@gmail.com>
2026-07-22 15:25:46 +0800

    dir的path为空用code填充

--- src/main/java/com/moyu/boot/system/service/impl/UserCenterServiceImpl.java
+++ src/main/java/com/moyu/boot/system/service/impl/UserCenterServiceImpl.java
@@ -132,12 +132,12 @@ public class UserCenterServiceImpl implements UserCenterService {
         List<SysResource> userMenuList = CollectionUtil.newArrayList();
         menuList.forEach(sysMenu -> {
             if (ResourceTypeEnum.MODULE.getCode().equals(sysMenu.getResourceType())) {
+                userMenuList.add(sysMenu);
+            } else if (ResourceTypeEnum.DIR.getCode().equals(sysMenu.getResourceType())) {
                 // 前端的路由对象path不能为空
                 if (ObjectUtil.isEmpty(sysMenu.getPath())) {
                     sysMenu.setPath(StrUtil.SLASH + sysMenu.getCode());
                 }
-                userMenuList.add(sysMenu);
-            } else if (ResourceTypeEnum.DIR.getCode().equals(sysMenu.getResourceType())) {
                 userMenuList.add(sysMenu);
             } else {
                 // 有权限才添加(菜单、内链、外链等)
@@ -243,9 +243,9 @@ public class UserCenterServiceImpl implements UserCenterService {
                     extra.put("path", menu.getPath());
                     extra.put("component", menu.getComponent());
                     if (ResourceTypeEnum.DIR.equals(resourceType)) {
-                        extra.put("redirect", menu.getLink());
+                        extra.put("redirect", menu.getPath());
                     } else if (ResourceTypeEnum.MODULE.equals(resourceType)) {
-                        extra.put("redirect", menu.getLink());
+                        extra.put("redirect", menu.getPath());
                     }
                     Meta meta = new Meta();
                     meta.setIcon(menu.getIcon());
