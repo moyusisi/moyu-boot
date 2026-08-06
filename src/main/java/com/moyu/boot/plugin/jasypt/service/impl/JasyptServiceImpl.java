c9a27462fd7f2e7e844ed404813c26c462f3a378
shisong <rgkjhshi@gmail.com>
2026-08-06 12:33:30 +0800

    Update JasyptServiceImpl.java

--- src/main/java/com/moyu/boot/plugin/jasypt/service/impl/JasyptServiceImpl.java
+++ src/main/java/com/moyu/boot/plugin/jasypt/service/impl/JasyptServiceImpl.java
@@ -1,10 +1,13 @@
 package com.moyu.boot.plugin.jasypt.service.impl;
 
+import com.moyu.boot.common.core.enums.ResultCodeEnum;
+import com.moyu.boot.common.core.exception.BusinessException;
 import com.moyu.boot.plugin.jasypt.model.param.EncryptorParam;
 import com.moyu.boot.plugin.jasypt.service.JasyptService;
 import lombok.extern.slf4j.Slf4j;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.EnvironmentStringPBEConfig;
+import org.jasypt.exceptions.EncryptionInitializationException;
 import org.springframework.stereotype.Service;
 
 /**
@@ -33,7 +36,17 @@ public class JasyptServiceImpl implements JasyptService {
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         encryptor.setConfig(config);
         // 加密
-        return encryptor.encrypt(param.getPlainText());
+        String encryptedText = null;
+        try {
+            encryptedText = encryptor.encrypt(param.getPlainText());
+        } catch (EncryptionInitializationException e) {
+            // 初始化阶段就失败，还没真正加解密，如密码为空、算法参数不合法等
+            throw new BusinessException(ResultCodeEnum.INVALID_PARAMETER_ERROR, "");
+        } catch (Exception e) {
+            // 解密失败：密码错误、salt不对、算法不匹配等或其他问题
+            throw new BusinessException(ResultCodeEnum.SYSTEM_ERROR.getCode(), "加密失败");
+        }
+        return encryptedText;
     }
 
     @Override
@@ -46,6 +59,16 @@ public class JasyptServiceImpl implements JasyptService {
         StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
         encryptor.setConfig(config);
         // 解密
-        return encryptor.decrypt(param.getEncryptedText());
+        String plainText = null;
+        try {
+            plainText = encryptor.decrypt(param.getEncryptedText());
+        } catch (EncryptionInitializationException e) {
+            // 初始化阶段就失败，还没真正加解密，如密码为空、算法参数不合法等
+            throw new BusinessException(ResultCodeEnum.INVALID_PARAMETER_ERROR, "");
+        } catch (Exception e) {
+            // 解密失败：密码错误、salt不对、算法不匹配等或其他问题
+            throw new BusinessException(ResultCodeEnum.SYSTEM_ERROR.getCode(), "解密失败");
+        }
+        return plainText;
     }
 }
