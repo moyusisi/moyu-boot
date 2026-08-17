999ffde36922733d17a71a5976f8cccc6e79f83b
shisong <rgkjhshi@gmail.com>
2026-08-17 16:57:36 +0800

    Update third_party_mysql.sql

--- sql/mysql/plugin/third_party_mysql.sql
+++ sql/mysql/plugin/third_party_mysql.sql
@@ -1,4 +1,4 @@
--- 三方集成接口表(可选)
+-- 三方接口表(可选)
 drop table if exists third_party_api;
 create table third_party_api
 (
@@ -28,4 +28,28 @@ create table third_party_api
 ) ENGINE = InnoDB
   CHARACTER SET = utf8mb4
   COLLATE = utf8mb4_general_ci
-  AUTO_INCREMENT = 100 COMMENT = '三方集成接口表';
+  AUTO_INCREMENT = 100 COMMENT = '三方接口表';
+
+-- 三方应用表(可选)
+drop table if exists third_party_app;
+create table third_party_app
+(
+    `id`          BIGINT NOT NULL AUTO_INCREMENT COMMENT '主键id',
+    `app_key`     VARCHAR(64) DEFAULT NULL COMMENT '应用标识',
+    `app_name`    VARCHAR(64) DEFAULT NULL COMMENT '应用名称',
+    `app_secret`  VARCHAR(64) DEFAULT NULL COMMENT '应用密钥',
+    `digest_algo` VARCHAR(16) DEFAULT NULL COMMENT '签名算法',
+
+    `ext_json`    TEXT        DEFAULT NULL COMMENT '扩展信息',
+    `remark`      TEXT        DEFAULT NULL COMMENT '备注',
+    `deleted`     TINYINT     DEFAULT 0 COMMENT '删除标志（0未删除  1已删除）',
+    `create_time` DATETIME    DEFAULT NULL COMMENT '创建时间',
+    `create_by`   VARCHAR(32) DEFAULT NULL COMMENT '创建人',
+    `update_time` DATETIME    DEFAULT NULL COMMENT '修改时间',
+    `update_by`   VARCHAR(32) DEFAULT NULL COMMENT '修改人',
+    primary key (`id`),
+    UNIQUE INDEX `uniq_app_key` (`app_key`)
+) ENGINE = InnoDB
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci
+  AUTO_INCREMENT = 100 COMMENT = '三方应用表';
