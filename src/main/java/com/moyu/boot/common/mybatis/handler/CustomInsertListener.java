1615803bb3e399b01a02a68a8346c2359acf3765
shisong <rgkjhshi@gmail.com>
2026-08-28 12:09:38 +0800

    UpdateListener填充非BaseEntity对象

--- src/main/java/com/moyu/boot/common/mybatis/handler/CustomInsertListener.java
+++ src/main/java/com/moyu/boot/common/mybatis/handler/CustomInsertListener.java
@@ -1,5 +1,7 @@
 package com.moyu.boot.common.mybatis.handler;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.ReflectUtil;
 import com.moyu.boot.common.authZ.util.LoginUserUtils;
 import com.moyu.boot.common.core.model.BaseEntity;
 import com.mybatisflex.annotation.InsertListener;
@@ -17,6 +19,9 @@ public class CustomInsertListener implements InsertListener {
 
     @Override
     public void onInsert(Object o) {
+        if (o == null) {
+            return;
+        }
         if (o instanceof BaseEntity) {
             BaseEntity entity = (BaseEntity) o;
             if (entity.getCreateTime() == null) {
@@ -24,6 +29,16 @@ public class CustomInsertListener implements InsertListener {
             }
             if (entity.getCreateBy() == null) {
                 entity.setCreateBy(LoginUserUtils.getUsername());
+            }
+        } else {
+            // 字段存在且值为null则填充
+            if (ReflectUtil.hasField(o.getClass(), BaseEntity.CREATE_TIME)
+                    && BeanUtil.getFieldValue(o, BaseEntity.CREATE_TIME) == null) {
+                BeanUtil.setFieldValue(o, BaseEntity.CREATE_TIME, new Date());
+            }
+            if (ReflectUtil.hasField(o.getClass(), BaseEntity.CREATE_BY)
+                    && BeanUtil.getFieldValue(o, BaseEntity.CREATE_BY) == null) {
+                BeanUtil.setFieldValue(o, BaseEntity.CREATE_BY, LoginUserUtils.getUsername());
             }
         }
     }
--- src/main/java/com/moyu/boot/common/mybatis/handler/CustomUpdateListener.java
+++ src/main/java/com/moyu/boot/common/mybatis/handler/CustomUpdateListener.java
@@ -1,5 +1,7 @@
 package com.moyu.boot.common.mybatis.handler;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.ReflectUtil;
 import com.moyu.boot.common.authZ.util.LoginUserUtils;
 import com.moyu.boot.common.core.model.BaseEntity;
 import com.mybatisflex.annotation.UpdateListener;
@@ -16,6 +18,9 @@ import java.util.Date;
 public class CustomUpdateListener implements UpdateListener {
     @Override
     public void onUpdate(Object o) {
+        if (o == null) {
+            return;
+        }
         if (o instanceof BaseEntity) {
             BaseEntity entity = (BaseEntity) o;
             if (entity.getUpdateTime() == null) {
@@ -23,6 +28,16 @@ public class CustomUpdateListener implements UpdateListener {
             }
             if (entity.getUpdateBy() == null) {
                 entity.setUpdateBy(LoginUserUtils.getUsername());
+            }
+        } else {
+            // 字段存在且值为null则填充
+            if (ReflectUtil.hasField(o.getClass(), BaseEntity.UPDATE_TIME)
+                    && BeanUtil.getFieldValue(o, BaseEntity.UPDATE_TIME) == null) {
+                BeanUtil.setFieldValue(o, BaseEntity.UPDATE_TIME, new Date());
+            }
+            if (ReflectUtil.hasField(o.getClass(), BaseEntity.UPDATE_BY)
+                    && BeanUtil.getFieldValue(o, BaseEntity.UPDATE_BY) == null) {
+                BeanUtil.setFieldValue(o, BaseEntity.UPDATE_BY, LoginUserUtils.getUsername());
             }
         }
     }
