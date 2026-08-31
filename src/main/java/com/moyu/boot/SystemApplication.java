8349c284fcf6134b646a3a36439f733b544db22b
shisong <rgkjhshi@gmail.com>
2026-08-31 18:46:28 +0800

    AutoConfiguration

--- src/main/java/com/moyu/boot/SystemApplication.java
+++ src/main/java/com/moyu/boot/SystemApplication.java
@@ -1,6 +1,5 @@
 package com.moyu.boot;
 
-import com.moyu.boot.common.core.annotation.EnableCommonLog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author shisong
  * @since 2023-02-21
  */
-@EnableCommonLog
-//@MapperScan(basePackages = {"com.moyu.boot.**.mapper"})
 @SpringBootApplication
 public class SystemApplication {
 
